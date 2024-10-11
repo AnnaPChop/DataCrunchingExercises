e8ff487d197a1fbf4d8190a5390696256ddcdf09
AnnaPChop <annapicn@gmail.com>
2024-10-11 14:02:38 -0600

    Update Long_exercise1.py

--- Long_exercise1.py
+++ Long_exercise1.py
@@ -32,5 +32,46 @@ else:
 #Result
 valor p: 0.0010501061456827461
 Rechazamos la hipótesis nula
+--------------------
+'''Tenemos dos datasets: la profundidad de la visita al sitio web de diferentes grupos de usuarios para los meses de verano y otoño. 
+Prueba la hipótesis de que las profundidades de visita de los sitios web son iguales. Por ejemplo, puede ser que en verano los visitantes no se sumerjan tanto en el 
+contenido, lo que sería algo a tener en cuenta al planificar una campaña publicitaria para los meses de verano. Vamos a establecer el nivel de significación en 0.05.
+No esperamos que las varianzas sean las mismas así que establece el parámetro equal_var en False.  Puedes ejecutar np.var(pages_per_session_autumn) 
+etcétera para verificar la varianza del conjunto.'''
+#Código
+from scipy import stats as st
+import numpy as np
+
+pages_per_session_autumn = [7.1, 7.3, 9.8, 7.3, 6.4, 10.5, 8.7, 
+                            17.5, 3.3, 15.5, 16.2, 0.4, 8.3, 
+                            8.1, 3.0, 6.1, 4.4, 18.8, 14.7, 16.4, 
+                            13.6, 4.4, 7.4, 12.4, 3.9, 13.6, 
+                            8.8, 8.1, 13.6, 12.2]
+pages_per_session_summer = [12.1, 24.3, 6.4, 19.9, 19.7, 12.5, 17.6, 
+                            5.0, 22.4, 13.5, 10.8, 23.4, 9.4, 3.7, 
+                            2.5, 19.8, 4.8, 29.0, 1.7, 28.6, 16.7, 
+                            14.2, 10.6, 18.2, 14.7, 23.8, 15.9, 16.2, 
+                            12.1, 14.5]
+
+# tu código va debajo
+
+alpha = 0.05  # el nivel de significancia estadística crítica
+
+results = st.ttest_ind(
+    pages_per_session_autumn, pages_per_session_summer, equal_var=False
+)
+
+print('valor p:', results.pvalue)
+
+if results.pvalue < alpha:
+    print("Rechazamos la hipótesis nula")
+else:
+    print("No podemos rechazar la hipótesis nula")
+  -------------------------
+#Result
+valor p: 0.002200163165401993
+Rechazamos la hipótesis nula
+-------------------------------
+
 
 
